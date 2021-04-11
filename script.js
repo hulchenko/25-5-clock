718a1a5491fcf28fb2077c5341aee2f83e9b230e
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-04-11 11:05:24 -0600

    add code

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
 function App() {
-  return <h1>Hi</h1>;
+  const [displayTime, setDisplayTime] = React.useState(25 * 60);
+
+  const formatTime = (time) => {
+    //main timer component
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    return (
+      (minutes < 10 ? '0' + minutes : minutes) +
+      ':' +
+      (seconds < 10 ? '0' + seconds : seconds)
+    );
+  };
+
+  return (
+    <div>
+      <Length
+        title={'break length'}
+        changeTime={null}
+        type={'break'}
+        time={null}
+        formatTime={formatTime}
+      />
+      <h1>{formatTime(displayTime)}</h1>
+    </div>
+  );
 }
 
+function Length({ title, changeTime, type, time, formatTime }) {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <div className="time-sets">
+        <button className="btn-small deep-purple lighten-2">
+          <i className="material-icons">arrow_downward</i>
+        </button>
+      </div>
+    </div>
+  );
+}
 ReactDOM.render(<App />, document.getElementById('root'));
